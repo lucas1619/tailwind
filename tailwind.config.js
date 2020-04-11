4710335170b0b508ab8b79b227e5513b7b51afa8
Lucas Moreno <lucas.moreno.olivos@gmail.com>
2020-04-11 17:14:26 -0500

    curso tailwind finalizado

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#f3c614',
+        'secondary' : '#353535'
+      },
+      height:{
+        '80': '20rem',
+        '100':'25rem'
+      }
+    },
+  },
+  variants: {
+    opacity:['responsive', 'hover', 'focus', 'disabled'],
+  },
+  plugins: [],
+}
